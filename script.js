4fcef5238fadb6b107b2923c95699271bce93469
kenkun499be <kenkun499be@outlook.jp>
2025-09-12 11:32:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -26,10 +26,10 @@ document.addEventListener("keyup", (e) => {
 
 function update() {
   // 押されているキーに応じて移動
-  if (keys["ArrowUp"])    player.y -= player.speed;
-  if (keys["ArrowDown"])  player.y += player.speed;
-  if (keys["ArrowLeft"])  player.x -= player.speed;
-  if (keys["ArrowRight"]) player.x += player.speed;
+  if (keys["ArrowUp","w"])    player.y -= player.speed;
+  if (keys["ArrowDown","s"])  player.y += player.speed;
+  if (keys["ArrowLeft","a"])  player.x -= player.speed;
+  if (keys["ArrowRight","d"]) player.x += player.speed;
 }
 
 function draw() {
