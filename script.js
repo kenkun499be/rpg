c305d99ea879ea18670fd6de48898693ae71dc13
kenkun499be <kenkun499be@outlook.jp>
2025-09-12 12:13:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -44,3 +44,14 @@ function draw() {
   requestAnimationFrame(draw);
 }
 draw();
+
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
